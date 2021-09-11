f9ef5dc7576ce599e34478d18c01329336b132be
TrungNEMO <trungnemo@gmail.com>
2021-09-11 11:42:25 +0700

    Style the add form by a customized PostAdd in the forms.py

---
+++ blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Post 
+
+#Customized form for the Post model
+class PostForm(forms.ModelForm):
+    # Class to holde the infor for the Form: Model, Model Fields
+    class Meta:
+        model = Post 
+        fields = ('title', 'title_tag', 'author', 'body')
+        # dictionary to specify the kind of html input types for each fields
+        widgets = {
+            #from.[TextInput, Select, Textarea are the form input types]
+            # attrs={'class':'form-control'}: is the html class style sheet from bootstrap
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Blog Title'}),
+            'title_tag': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Blog Tag'}),
+            'author': forms.Select(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Blog Body for long text'})
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+#Use the customized form
+from .forms import PostForm
 # #Home 1
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -14,11 +16,20 @@ class HomeView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blogdetail.html'
-# Post Add to add a new post
+# # Post Add to add a new post
+# class BlogAddView(CreateView):
+#     model = Post
+#     template_name = 'blogadd.html'
+#     # Soame specified fields in the form only
+#     # fields = ('title','title_tag', 'body','author')
+#     # All fields will appear in the form
+#     fields = '__all__'
+
+# Modify the BlogAddView to use the custom form: PostForm
 class BlogAddView(CreateView):
     model = Post
+    #Just Add this to use the custom form
+    #and remove the fields = (cause the PostForm take care all of the fields)
+    form_class = PostForm 
     template_name = 'blogadd.html'
-    # Soame specified fields in the form only
-    # fields = ('title','title_tag', 'body','author')
-    # All fields will appear in the form
-    fields = '__all__'
+    
