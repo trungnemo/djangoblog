7396072337971949de118169b608deaf0f3f9542
TrungNEMO <trungnemo@gmail.com>
2021-09-20 16:53:54 +0700

    Ini the Profile edit: html, views, urls, link

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView
 #members views/urls here
 urlpatterns = [
     path('members/',UserRegisterView.as_view(), name = 'register'),
+    path('members/profile/',UserEditView.as_view(), name = 'editprofile'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 # 1 User Registration
@@ -10,3 +10,12 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+#2 For User Profile edit
+class UserEditView(generic.UpdateView):
+    #form_class = UserCreationForm
+    form_class = UserChangeForm
+    template_name = 'registration/profileedit.html'
+    success_url = reverse_lazy('login')
+
+    def get_object(self):
+        return self.request.user
