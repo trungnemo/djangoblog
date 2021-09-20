c3fecd39f8d6a9373523223b7cffd907cccb47ae
TrungNEMO <trungnemo@gmail.com>
2021-09-20 09:02:45 +0700

    Add RichTextEditor

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Add this import for the add a new post 
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 #Post Category
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -19,7 +20,8 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255,default="")
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    body = models.TextField()
+    #body = models.TextField()
+    body = RichTextField(blank = True, null = True)
     post_date = models.DateField(auto_now_add = True)
     category = models.CharField(max_length=255, default="None")
     #ManyToMany Likes relations ships
