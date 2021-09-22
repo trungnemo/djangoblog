592fdd655acc9c23ad712d289b072c47931f70a4
TrungNEMO <trungnemo@gmail.com>
2021-09-22 12:01:55 +0700

    User Profile model with extra infor

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,17 @@ class Post(models.Model):
     def get_absolute_url(self):
         #return reverse('blogdetail', args=(str(self.id)))
         return reverse('home')
+
+# User Profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null = True, blank = True, upload_to = "images/profiles/")
+    website_url = models.CharField(max_length=255, null = True, blank = True)
+    facebook_url = models.CharField(max_length=255, null = True, blank = True)
+    twitter_url = models.CharField(max_length=255, null = True, blank = True)
+    instagram_url = models.CharField(max_length=255, null = True, blank = True)
+    pinterest_url = models.CharField(max_length=255, null = True, blank = True)
+
+    def __str__(self):
+        return str(self.user)
